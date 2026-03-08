60d12468db771821d2a253390f3c3f1c02c83fc5
judy <jgirlpower42@gmail.com>
2026-03-07 22:00:52 -0300

    outro

---
+++ Data.java
@@ -0,0 +1,9 @@
+public class Data {
+   public static void main (String[] args) {
+     int dia = 07;
+     String mes= "Março";
+     int ano =2026;
+     
+      System.out.println("Hoje é "+ dia + " de "  + mes + " de " + ano);
+   }
+}
